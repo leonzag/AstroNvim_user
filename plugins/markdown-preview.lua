b05f7339e63d100579d40826affb83d0eca6f939
Leonid Zagumennov <leonzag997@gmail.com>
2023-04-13 05:53:05 +0300

    MD preview

---
+++ plugins/markdown-preview.lua
@@ -0,0 +1,29 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function() vim.fn["mkdp#util#install"]() end,
+    config = function()
+        vim.api.nvim_exec(
+            [[
+                function! OpenMarkdownPreview(url)
+                    execute "silent ! firefox --new-window " . a:url
+                endfunction
+            ]],
+            true
+        )
+        vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+        -- set to 1, nvim will open the preview window after entering the markdown buffer
+        -- default: 0
+        vim.g.mkdp_auto_start = 0
+        -- set to 1, the nvim will auto close current preview window when change
+        -- from markdown buffer to another buffer
+        -- default: 1
+        vim.g.mkdp_auto_close = 0
+        -- recognized filetypes
+        -- these filetypes will have MarkdownPreview... commands
+        vim.g.mkdp_filetypes = { "markdown" }
+        -- set default theme (dark or light)
+        -- By default the theme is define according to the preferences of the system
+        vim.g.mkdp_theme = "dark"
+    end,
+}

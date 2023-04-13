5bd9a8d69fe91a98fbc4df07824ee2b3f01493eb
Leonid Zagumennov <leonzag997@gmail.com>
2023-04-13 05:41:20 +0300

    Refactor to AstroNvim v3 latest updates + add tools to autoload

--- plugins/gruvbox.lua
+++ plugins/gruvbox.lua
@@ -1,18 +1,24 @@
 local bg = "#232323"
 local fg = "#ebdbb2"
-return require("gruvbox").setup({
-    overrides = {
-        NormalFloat = {bg=bg},
-        Float = {bg=bg},
-        SignColumn = {bg = "none"},
-        CursorLineNr = {bg = "none"},
-        GruvboxYellowSign = {bg = "none"},
-        GruvboxPurpleSign = {bg = "none"},
-        GruvboxOrangeSign = {bg = "none"},
-        GruvboxGreenSign = {bg = "none"},
-        GruvboxBlueSign = {bg = "none"},
-        GruvboxAquaSign = {bg = "none"},
-        GruvboxRedSign = {bg = "none"},
-        StatusLine = {fg = fg, bg = bg},
+
+return {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        overrides = {
+            NormalFloat = { bg = bg },
+            Float = { bg = bg },
+            SignColumn = { bg = "none" },
+            CursorLineNr = { bg = "none" },
+            GruvboxYellowSign = { bg = "none" },
+            GruvboxPurpleSign = { bg = "none" },
+            GruvboxOrangeSign = { bg = "none" },
+            GruvboxGreenSign = { bg = "none" },
+            GruvboxBlueSign = { bg = "none" },
+            GruvboxAquaSign = { bg = "none" },
+            GruvboxRedSign = { bg = "none" },
+            StatusLine = { fg = fg, bg = bg },
+        },
     },
-})
+}
--- plugins/mason-null-ls.lua
+++ plugins/mason-null-ls.lua
@@ -1,11 +1,46 @@
+local null_ls = require "null-ls"
+
 return {
-    ensure_installed = {
-        "shellcheck",
-        "stylua",
-        "black",
-        "isort",
-        "prettierd",
-        "shfmt",
-        "shellcheck",
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+        ensure_installed = {
+            "stylua",
+            "jq",
+            "prettierd",
+            "shellcheck",
+            "shfmt",
+        },
+        handlers = {
+            flake8 = function()
+                null_ls.register(null_ls.builtins.diagnostics.flake8.with {
+                    extra_args = {
+                        "--max-line-length",
+                        "120",
+                        "--ignore",
+                        "E302 E305 W391 F401",
+                        -- expected 2 lines, blank line at end of file, imported but unused
+                    },
+                })
+            end,
+            prettierd = function()
+                require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+                    condition = function(utils)
+                        return utils.root_has_file "package.json"
+                            or utils.root_has_file ".prettierrc"
+                            or utils.root_has_file ".prettierrc.json"
+                            or utils.root_has_file ".prettierrc.js"
+                    end,
+                })
+            end,
+            eslint_d = function()
+                require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+                    condition = function(utils)
+                        return utils.root_has_file "package.json"
+                            or utils.root_has_file ".eslintrc.json"
+                            or utils.root_has_file ".eslintrc.js"
+                    end,
+                })
+            end,
+        },
     },
 }

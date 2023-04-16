0a1dfe879772b8f66938aea77cfb1f847e907c9a
Leonid Zagumennov <leonzag997@gmail.com>
2023-04-17 02:41:37 +0300

    add few nice plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,4 +1,32 @@
+-- local prefix_refactoring = "<leader>r"
+
 return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.diagnostics.trouble-nvim" },
+    {
+        "folke/trouble.nvim",
+        keys = {
+            { "gL", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+        },
+    },
+    { import = "astrocommunity.editing-support.todo-comments-nvim" },
+    {
+        "todo-comments.nvim",
+        opts = {
+            keywords = {
+                FIX = {
+                    alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR", "ERR" },
+                },
+            },
+        },
+        keys = {
+            { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+            { "[t",         function() require("todo-comments").jump_prev() end, desc = "Prev todo comment" },
+            { "<leader>fT", "<cmd>TodoTelescope<cr>",                            desc = "Search Todos" },
+        },
+    },
+    { import = "astrocommunity.scrolling.satellite-nvim" },
+    { import = "astrocommunity.editing-support.neogen" },
+    { import = "astrocommunity.indent.indent-tools-nvim" },
 }

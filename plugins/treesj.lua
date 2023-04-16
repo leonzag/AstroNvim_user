63700726b6c8389d3f68c305c03d934eb9a6a204
Leonid Zagumennov <leonzag997@gmail.com>
2023-04-17 02:41:05 +0300

    add new keymaps for TreesJoinToggle plugin

--- plugins/treesj.lua
+++ plugins/treesj.lua
@@ -1,6 +1,10 @@
 return {
     "Wansmer/treesj",
-    keys = { { "gs", "<CMD>TSJToggle<CR>", desc = "Toggle Treesitter Join" } },
+    keys = {
+        { "gs", "<CMD>TSJToggle<CR>", desc = "Toggle TreesJoin" },
+        { "J",  "<CMD>TSJToggle<CR>", desc = "Toggle TreesJoin" },
+        { "J",  "<CMD>TSJToggle<CR>", mode = { "v" },           desc = "Toggle TreesJoin" },
+    },
     cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
     opts = { use_default_keymaps = false },
 }

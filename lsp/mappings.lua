87b93ad70d2161d2aebbb3e0511f1790baa6a92e
Leonid Zagumennov <leonzag997@gmail.com>
2023-05-08 00:33:40 +0300

    add lspsaga and keymaps for it

--- lsp/mappings.lua
+++ lsp/mappings.lua
@@ -1,6 +1,20 @@
--- easily add or disable built in mappings added during LSP attaching
+local hover_doc = {}
+local line_diagnostic = {}
+
+local lspsaga_avail, _ = pcall(require, "lspsaga")
+if lspsaga_avail then
+    hover_doc = { false, desc = "Hover symbol details" }
+    line_diagnostic = { false, desc = "Hover diagnostics" }
+end
+
 return {
     n = {
-        -- ["<leader>lf"] = false -- disable formatting keymap
+        ["gd"] = { false, desc = "Definition" },
+        ["gdd"] = {
+            function() vim.lsp.buf.definition() end,
+            desc = "Go to Definition",
+        },
+        ["gl"] = line_diagnostic,
+        ["K"] = hover_doc,
     },
 }

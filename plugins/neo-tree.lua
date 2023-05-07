bbb1de07d2119f09920ac14bcd687de7e36cb6f3
Leonid Zagumennov <leonzag997@gmail.com>
2023-05-08 00:38:48 +0300

    + set of <always show files>

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -3,4 +3,11 @@ return {
     keys = {
         { "<A-e>", "<esc><cmd>Neotree toggle<cr>", mode = { "n", "v" }, desc = "Toggle Explorer" },
     },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                always_show = { ".github", ".gitignore", ".config", ".local", ".zsh" },
+            },
+        },
+    },
 }
